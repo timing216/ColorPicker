e108716d70bb4bf1440295874894c43922662a1c
Utaro Hayashi <utaro.hayashi@gmail.com>
2023-06-13 19:39:23 -0500

    Added ColorPicker with 2px black border css for picked color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useState } from "react";
+
 // Write your Color component here
+const Color = ({ color, selectedColor, selectColor }) => {
+  const isSelected = selectedColor === color ? "selected" : "";
+  return (
+    <div
+      className={`${color} ${isSelected}`}
+      onClick={() => selectColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+  const selectColor = (colorName) => {
+    setSelectedColor(colorName);
+  };
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="green"
+          selectedColor={selectedColor}
+          selectColor={selectColor}
+        />
+
+        <Color
+          color="yellow"
+          selectedColor={selectedColor}
+          selectColor={selectColor}
+        />
+        <Color
+          color="red"
+          selectedColor={selectedColor}
+          selectColor={selectColor}
+        />
+      </div>
     </div>
   );
 };
